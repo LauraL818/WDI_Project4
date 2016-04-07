c91c1d15da137b7ee6968480c29fca1dcf1f610b
Laura Leonard <Laura.Leonard818@gmail.com>
2016-04-06 20:53:06 -0700

    cleaned up code

--- public/js/userCtrl.js
+++ public/js/userCtrl.js
@@ -84,40 +84,18 @@
           ]
         }
 
-        // Chart.js Options
         vm.options =  {
-
-          // Sets the chart to be responsive
           responsive: true,
-
-          //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
           scaleBeginAtZero : true,
-
-          //Boolean - Whether grid lines are shown across the chart
           scaleShowGridLines : true,
-
-          //String - Colour of the grid lines
           scaleGridLineColor : "rgba(0,0,0,.05)",
-
-          //Number - Width of the grid lines
           scaleGridLineWidth : 1,
-
-          //Boolean - If there is a stroke on each bar
           barShowStroke : true,
-
-          //Number - Pixel width of the bar stroke
           barStrokeWidth : 2,
-
-          //Number - Spacing between each of the X value sets
           barValueSpacing : 5,
-
-          //Number - Spacing between data sets within X values
-          barDatasetSpacing : 1,
-
+          barDatasetSpacing : 1
         }
-
       }
-
     }
 
     function profileChart(){
